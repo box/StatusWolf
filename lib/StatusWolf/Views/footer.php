e0ca921c63549ff9465a0398f2b5a2cb4a75220f
Mark Troyer <disco@box.com>
2013-06-03 10:05:03 -0700

    Added dynamic load function for javascript

--- lib/StatusWolf/Views/footer.php
+++ lib/StatusWolf/Views/footer.php
@@ -11,8 +11,20 @@
  */
 ?>
 
-    <script type="text/javascript" src="<?php echo URL; ?>/app/js/lib/jquery.js"></script>
-    <script type="text/javascript" src="<?php echo URL; ?>/app/js/lib/bootstrap.js"></script>
+    <script type="text/javascript">
+      function loadScript(url, callback){
+
+        var script = document.createElement("script")
+        script.type = "text/javascript";
+
+        script.onload = function(){
+          callback();
+        };
+
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+      }
+    </script>
 
   </body>
 
