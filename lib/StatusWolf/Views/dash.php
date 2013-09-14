03b918560f6e9061a6b77198a51c4e49f3d213b6
Mark Troyer <disco@box.com>
2013-09-13 17:29:29 -0700

    Interface tweaks for the navbar

--- lib/StatusWolf/Views/dash.php
+++ lib/StatusWolf/Views/dash.php
@@ -166,9 +166,9 @@ foreach($widgets as $widget_key)
     var loaded_widgets = [];
     this.sw_url = '<?php echo URL; ?>';
 
-    $('#menu-placeholder').replaceWith('<div class="dashboard-menu dropdown menu-btn" id="dashboard-menu">');
+    $('#menu-placeholder').replaceWith('<div class="dashboard-menu left-button dropdown menu-btn" id="dashboard-menu">');
     $('#dashboard-menu').append('<span class="flexy" id="dashboard-menu-content" data-toggle="dropdown">')
-    $('#dashboard-menu-content').append('<span class="menu-label" id="dashboard-menu-label">Dashboard <span class="iconic iconic-play rotate-90">');
+    $('#dashboard-menu-content').append('<span class="menu-label" id="dashboard-menu-label">Dashboard</span>');
     $('#dashboard-menu').append('<ul class="dropdown-menu sub-menu-item" id="dashboard-menu-options" role="menu" aria-labelledby="dLabel">');
     $('#dashboard-menu-options').append('<li id="clear-dashboard-menu-choice"><a href="<?php echo URL; ?>dashboard/"><span>Clear</span></a></li>');
     $('#dashboard-menu-options').append('<li class="flexy dropdown" id="add-widget-menu-item"><span>Add Widget</span></span><span class="iconic iconic-play"></span></li>');
--- lib/StatusWolf/Views/navbar.php
+++ lib/StatusWolf/Views/navbar.php
@@ -11,23 +11,31 @@
  */
 ?>
 
-    <div class="navbar">
-      <link href="<?php echo URL; ?>app/css/nav.css?v=1" rel="stylesheet">
-      <div class="brand"><a href="<?php echo URL; ?>"><h4>StatusWolf</h4></a></div>
-      <div class="version"><p><?php $ver = SWConfig::read_values('statuswolf.version'); echo $ver; ?></p></div>
-      <div id="menu-placeholder"></div>
-      <div class="user-menu dropdown menu-btn" id="user-menu">
-        <form id="logout" method="post" action="<?php echo URL; ?>">
-          <input type="hidden" name="action" value="logout">
-          <span class="iconic iconic-user" style="padding-left: 10px;"></span><a data-toggle="dropdown"><span class="menu-label"><?php echo $this->usersession['friendly_name']; ?></span></a>
-          <ul class="dropdown-menu menu-left" id="user-menu-options" role="menu" aria-labelled-by="dLabel">
-            <li><a href="<?php echo URL; ?>account/my_settings"><span>My Settings</span></a></li>
-            <?php if ($_SESSION[SWConfig::read_values('auth.sessionName')]['data']['auto_login'] !== "true")
-                  {
-                    echo '<li onClick="$(\'#logout\').submit()"><span>Logout</span></li>';
-                  }
-            ?>
-          </ul>
-        </form>
-      </div>
-    </div>
+<div class="navbar">
+  <link href="<?php echo URL; ?>app/css/nav.css?v=1" rel="stylesheet">
+  <div class="brand dropdown menu-btn" id="nav-menu">
+    <span class="flexy" id="statuswolf-menu-content" data-toggle="dropdown">
+      <h4 class="menu-label" id="statuswolf-menu-content">StatusWolf</h4>
+      <span class="version"><p><?php $ver = SWConfig::read_values('statuswolf.version'); echo $ver ?></p></span>
+    </span>
+    <ul class="dropdown-menu" id="nav-menu-content" role="menu">
+      <li id="nav-home"><a href="<?php echo URL; ?>"><span class="iconic iconic-home"><span> Home</span></span></a></li>
+      <li id="nav-adhoc"><a href="<?php echo URL; ?>adhoc/"><span>Ad-Hoc Search</span></a></li>
+      <li id="nav-dashboard"><a href="<?php echo URL; ?>dashboard/"><span>Dashboard</span></a></li>
+    </ul>
+  </div><div id="menu-placeholder"></div>
+  <div class="user-menu dropdown menu-btn" id="user-menu">
+    <form id="logout" method="post" action="<?php echo URL; ?>">
+      <input type="hidden" name="action" value="logout">
+      <span class="iconic iconic-user" style="padding-left: 10px;"></span><a data-toggle="dropdown"><span class="menu-label"><?php echo $this->usersession['friendly_name']; ?></span></a>
+      <ul class="dropdown-menu menu-left" id="user-menu-options" role="menu" aria-labelled-by="dLabel">
+        <li><a href="<?php echo URL; ?>account/my_settings"><span>My Settings</span></a></li>
+        <?php if ($_SESSION[SWConfig::read_values('auth.sessionName')]['data']['auto_login'] !== "true")
+              {
+                echo '<li onClick="$(\'#logout\').submit()"><span>Logout</span></li>';
+              }
+        ?>
+      </ul>
+    </form>
+  </div>
+</div>
