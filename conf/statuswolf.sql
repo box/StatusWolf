944f006726b9df3fa57ef07ef809388876f01b0f
Mark Troyer <disco@box.com>
2013-10-07 13:15:09 -0700

    Updates to database scripts for new version

--- conf/statuswolf.sql
+++ conf/statuswolf.sql
@@ -1,7 +1,3 @@
--- MySQL dump 10.13  Distrib 5.5.28, for osx10.8 (i386)
---
--- Host: localhost    Database: statuswolf
--- ------------------------------------------------------
 -- Server version	5.5.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -113,6 +109,21 @@ CREATE TABLE `user_map` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+--
+-- Table structure for table `dashboard_rank`
+--
+
+DROP TABLE IF EXISTS `dashboard_rank`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dashboard_rank` (
+  `id` VARCHAR(32),
+  `count` INT(15) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -120,5 +131,3 @@ CREATE TABLE `user_map` (
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-07-26 11:02:34
