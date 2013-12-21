ef1dfa23bf4ac3e0884873f9280d2038f71660e6
Mark Troyer <disco@box.com>
2013-12-20 16:32:48 -0800

    Fixed issue loading new data tables js lib on case-sensitive filesystems. OS X strikes again.

--- lib/StatusWolf/Views/dash.php
+++ lib/StatusWolf/Views/dash.php
@@ -101,7 +101,7 @@ $db_conf = $_sw_conf['session_handler'];
 <script type="text/javascript" src="<?php echo URL; ?>app/js/lib/md5.js"></script>
 <script type="text/javascript" src="<?php echo URL; ?>app/js/status_wolf_colors.js"></script>
 <script type="text/javascript" src="<?php echo URL; ?>app/js/lib/jquery.autocomplete.js"></script>
-<script type="text/javascript" src="<?php echo URL; ?>app/js/lib/jquery.datatables.min.js"></script>
+<script type="text/javascript" src="<?php echo URL; ?>app/js/lib/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="<?php echo $_sw_conf['graphing']['d3_location'] ?>"></script>
 
 <?php
