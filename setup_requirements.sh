dc2805436709f873d4aa97f96a0b5081f07dbcda
Mark Troyer <disco@box.com>
2014-08-12 10:20:34 -0700

    Fixing a couple of bugs in the setup script, renamed

--- setup_requirements.sh
+++ setup_requirements.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
+WWW_USER='apache'
+
+echo -n "Which user does your web server run as? [apache]: "
+read USERIN
+
+if [ ! -z $USERIN ]; then
+    WWW_USER=$USERIN
+fi
+
+WWW_GROUP=$(id -gn ${WWW_USER})
+
+mkdir app/log
+chown ${WWW_USER}:${WWW_GROUP} app/log
+chown -R ${WWW_USER}:${WWW_GROUP} conf
+
 cd app/static/js/lib
 
 # Get JQuery
 /usr/bin/wget http://code.jquery.com/jquery-1.11.1.min.js
 ln -s jquery-1.11.1.min.js jquery.js
+ln -s jquery-1.11.1.min.js jquery.min.js
 
 # Get JQuery DataTables
 /usr/bin/wget http://datatables.net/releases/DataTables-1.10.0.zip
@@ -14,11 +30,11 @@ rm -rf DataTables-1.10.0
 rm DataTables-1.10.0.zip
 
 # Get MagnificPopup
-/usr/bin/wget https://raw.githubusercontent.com/dimsemenov/Magnific-Popup/master/dist/jquery.magnific-popup.min.js
+/usr/bin/wget --no-check-certificate https://raw.githubusercontent.com/dimsemenov/Magnific-Popup/master/dist/jquery.magnific-popup.min.js
 ln -s jquery.magnific-popup.min.js magnific-popup.js
 
 # Get JQuery Autocomplete
-/usr/bin/wget https://raw.githubusercontent.com/devbridge/jQuery-Autocomplete/master/dist/jquery.autocomplete.min.js
+/usr/bin/wget --no-check-certificate https://raw.githubusercontent.com/devbridge/jQuery-Autocomplete/master/dist/jquery.autocomplete.min.js
 ln -s jquery.autocomplete.min.js jquery.autocomplete.js
 
 # Get Bootstrap
