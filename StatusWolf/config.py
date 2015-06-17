d76d78fcc8f39cce3e38f40c751fd4400a145f48
Mark Troyer <disco@hipmunk.com>
2015-06-17 10:38:23 -0700

    enable loadable configs

--- StatusWolf/config.py
+++ StatusWolf/config.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 class config(object):
 
     app = {
@@ -17,8 +20,24 @@ class config(object):
         'AUTH_LIB': 'ldap',
     }
 
+    datasource = {}
+
     logging = {
         'LOGLEVEL': 'INFO',
         'LOG_FORMAT': '%(asctime)s %(name)s [%(levelname)s]: %(message)s:',
-        'APP_LOG_FILE': '/var/tmp/sw_app.log',
+        'APP_LOG_FILE': 'sw_app.log',
     }
+
+
+loaded_config = {}
+for config_file in os.listdir('config'):
+    if '.json' in config_file and 'example' not in config_file:
+        cf = open(os.path.join('config', config_file), 'r')
+        loaded_config.update(json.load(cf))
+
+for conf_attr in loaded_config:
+    if hasattr(config, conf_attr):
+        master_object = getattr(config, conf_attr)
+        master_object.update(loaded_config[conf_attr])
+    else:
+        setattr(config, conf_attr, loaded_config[conf_attr])
